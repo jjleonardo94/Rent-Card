2ff709ef2cc650e6c0b8707eaae1dc4462986c21
Juan Jose Leonardo Amparo <jjleonardoamparo@gmail.com>
2023-06-12 19:55:22 -0400

    initial commit

---
+++ components/index.tsx
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+// import ShowMore from "./ShowMore";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  Navbar,
+  CustomFilter,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  ShowMore,
+};
